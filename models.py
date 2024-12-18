477e30fa2cc086754a431851d0c50817f5e5d2ef
ken sofware engineer <mainakenken188@gmail.com>
2024-12-18 12:56:29 +0300

    i ru alembic

--- models.py
+++ models.py
@@ -1,29 +1,28 @@
-#models needed here are 2 the Bank branch model and Account model
-#
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship, declarative_base
 
-Base =declarative_base()
- 
+Base = declarative_base()
+
 class Branch(Base):
-    __tablename__ ="Branch"
+    __tablename__ = "Branch"
     id = Column(Integer, primary_key=True)
-    name= Column(String,nullable=false)
-    location= Column(String, nullable=false)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
 
-    accounts= relationship("BankAccounts", back_populates="branch", cascade="all")
+    accounts = relationship("BankAccount", back_populates="branch", cascade="all, delete-orphan")
+
     def __repr__(self):
-        return f"Branch(id= {self.id} name ={self.name} location={self.location})"
+        return f"Branch(id={self.id}, name={self.name}, location={self.location})"
 
 
-class BankAccounts(Base):
-    __tablename__ ="BankAccounts"
+class BankAccount(Base):
+    __tablename__ = "BankAccounts"
     id = Column(Integer, primary_key=True)
-    holdersname= Column(String,nullable=false)
+    holdersname = Column(String, nullable=False)
     balance = Column(Float, default=0.0)
-    branch_id = Column(Integer, ForeignKey("Branch.id"))
+    branch_id = Column(Integer, ForeignKey("Branch.id"), nullable=False)
     
-    branch = relationship("Branch", back_populates="BankAccounts")
+    branch = relationship("Branch", back_populates="accounts")
 
     def __repr__(self):
-        return f"BankAccount(id={self.id}, name{self.holdersname},branch={self.branch} branchid{self.branch_id})"
+        return f"BankAccount(id={self.id}, holdersname={self.holdersname}, branch={self.branch}, branch_id={self.branch_id})"
